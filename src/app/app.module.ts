f2d042a7f4f9297126667b55de16da4d9e96db6f
Karen Abril Grijalva Nieves <karenieves@outlook.com>
2019-05-20 18:53:49 -0600

    feat: se agrega componente models

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ModelsComponent } from './models/models.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ModelsComponent
   ],
   imports: [
     BrowserModule,
