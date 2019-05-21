30a0b58c3cc8cf65d06b95a431513366e1ed23e1
Karen Abril Grijalva Nieves <karenieves@outlook.com>
2019-05-20 18:56:03 -0600

    feat: se agrega componente services

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ModelsComponent } from './models/models.component';
+import { ServicesComponent } from './services/services.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ModelsComponent
+    ModelsComponent,
+    ServicesComponent
   ],
   imports: [
     BrowserModule,
